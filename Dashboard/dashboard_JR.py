3ccfce4262db5b88b01c0051752aa426ec9be5ae
Jelang Ramadhan <144755170+JelangR@users.noreply.github.com>
2025-02-19 11:41:43 +0700

    Update dashboard_JR.py

--- Dashboard/dashboard_JR.py
+++ Dashboard/dashboard_JR.py
@@ -106,7 +106,7 @@ st.subheader("Customer's favorite season	:sunny:	:cloud:")
 fig, ax = plt.subplots(figsize=(16, 8))
 ax.plot(
     daily_orders_df["dteday"],
-    daily_orders_df["cnt"],
+    daily_orders_df["bulan"],
     marker='o', 
     linewidth=2,
     color="#90CAF9"
