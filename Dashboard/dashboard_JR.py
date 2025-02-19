a7a8b26a9c35aceebbcd13a8f6862e52115c2c0a
Jelang Ramadhan <144755170+JelangR@users.noreply.github.com>
2025-02-19 11:04:05 +0700

    Update dashboard_JR.py

--- Dashboard/dashboard_JR.py
+++ Dashboard/dashboard_JR.py
@@ -46,6 +46,8 @@ with st.sidebar:
         max_value=max_date,
         value=[min_date, max_date]
     )
+main_df = day_df[(day_df["dteday"] >= str(start_date)) & 
+                (day_df["dteday"] <= str(end_date))]
 with st.sidebar:
     st.subheader("Background")
     st.write(
